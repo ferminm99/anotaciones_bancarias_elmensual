35d32ea730364f34512df6a7af42a29d03650e56
Ferminm99 <ferminmorenoz99@gmail.com>
2024-10-23 22:15:41 -0300

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
+  safelist: ["text-green-500", "text-red-500", "text-blue-500"],
   plugins: [],
-}
+};
