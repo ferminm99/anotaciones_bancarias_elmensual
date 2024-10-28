ea6cb88c7e208b1afe0aff7820f7c11fb81a723d
Ferminm99 <ferminmorenoz99@gmail.com>
2024-10-28 16:19:04 -0300

    arreglo api para que ande deploy

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,15 +1,15 @@
 import axios from "axios";
 import { Transaction, CreateTransaction, Bank } from "../types";
 
-//Configura Axios con la baseURL
-const api = axios.create({
-  baseURL: "http://localhost:3001", // Solo la baseURL, sin especificar la ruta completa
-});
-
-// // Configura Axios con la baseURL
+// //Configura Axios con la baseURL
 // const api = axios.create({
-//   baseURL: process.env.NEXT_PUBLIC_API_URL, // Solo la baseURL, sin especificar la ruta completa
+//   baseURL: "http://localhost:3001", // Solo la baseURL, sin especificar la ruta completa
 // });
+
+// Configura Axios con la baseURL
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001", // Solo la baseURL, sin especificar la ruta completa
+});
 
 console.log("Usando API URL:", process.env.NEXT_PUBLIC_API_URL);
 
